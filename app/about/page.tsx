944b06f6e53a8c92fd4ceccbe672a6368c942214
Dahiru Usman Hassan <usmandahirub@gmail.com>
2026-06-02 10:46:11 +0100

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -72,19 +72,12 @@ export default function AboutPage() {
           </div>
 
           <div className="team-grid">
-            <div className="leader-card">
-              <img className="leader-img" src="/image 10.png" alt="Alhaji Danjuma Ochidi" />
-              <div className="leader-info">
-                <h3 className="leader-name">Alhaji Danjuma Ochidi</h3>
-                <p className="leader-title">H.O.D Reform Services Coordination & Innovation Department (RSCID)</p>
-              </div>
-            </div>
 
             <div className="leader-card">
-              <img className="leader-img" src="/image 11.png" alt="Dr. Abokwara-Adjekota Alero" />
+              <img className="leader-img" src="/image 5.png" alt="Mrs. Ada Veronica Uche" />
               <div className="leader-info">
-                <h3 className="leader-name">Dr. Abokwara-Adjekota Alero</h3>
-                <p className="leader-title">H.O.D Operation & Rescue</p>
+                <h3 className="leader-name">Mrs. Ada Veronica Uche</h3>
+                <p className="leader-title">H.O.D Special Services</p>
               </div>
             </div>
 
@@ -97,14 +90,14 @@ export default function AboutPage() {
             </div>
 
             <div className="leader-card">
-              <img className="leader-img" src="/image 33.png" alt="Alhaji Usman Furfuri" />
+              <img className="leader-img" src="/image 11.png" alt="Dr. Abokwara-Adjekota Alero" />
               <div className="leader-info">
-                <h3 className="leader-name">Alhaji Ibrahim Usman Furfuri</h3>
-                <p className="leader-title">H.O.D Human Resources</p>
+                <h3 className="leader-name">Dr. Abokwara-Adjekota Alero</h3>
+                <p className="leader-title">H.O.D Operation & Rescue</p>
               </div>
             </div>
 
-            <div className="leader-card">
+             <div className="leader-card">
               <img className="leader-img" src="/image 34.png" alt="Mr. Ebiwari Elehibiri" />
               <div className="leader-info">
                 <h3 className="leader-name">Mr. Ebiwari Elehibiri</h3>
@@ -113,12 +106,22 @@ export default function AboutPage() {
             </div>
 
             <div className="leader-card">
-              <img className="leader-img" src="/image 5.png" alt="Mrs. Ada Veronica Uche" />
+              <img className="leader-img" src="/image 10.png" alt="Alhaji Danjuma Ochidi" />
               <div className="leader-info">
-                <h3 className="leader-name">Mrs. Ada Veronica Uche</h3>
-                <p className="leader-title">H.O.D Special Services</p>
+                <h3 className="leader-name">Alhaji Danjuma Ochidi</h3>
+                <p className="leader-title">H.O.D Reform Services Coordination & Innovation Department (RSCID)</p>
               </div>
             </div>
+
+            <div className="leader-card">
+              <img className="leader-img" src="/image 33.png" alt="Alhaji Usman Furfuri" />
+              <div className="leader-info">
+                <h3 className="leader-name">Alhaji Ibrahim Usman Furfuri</h3>
+                <p className="leader-title">H.O.D Human Resources</p>
+              </div>
+            </div>
+
+            
           </div>
         </div>
       </section>
