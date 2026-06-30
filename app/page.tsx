ad5d0955dad545ed58c42cd78c07e99e13629df4
Dahiru Usman Hassan <usmandahirub@gmail.com>
2026-06-30 10:17:33 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import sublink from "@/components/sublink"
 import Hero from "@/components/Hero";
 import EventsSlideshow from "@/components/EventsSlideshow";
 import UpcomingPrograms from "@/components/UpcomingPrograms";
